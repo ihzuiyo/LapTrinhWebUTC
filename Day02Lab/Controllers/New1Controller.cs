b259353e63cde86351e51d6d3dcc3e56c3e98c3f
ihzuiyo <danghuy200305@gmail.com>
2025-08-28 17:29:21 +0700

    Dang Hoang Huy 231230797 Day02Lab

---
+++ Day02Lab/Controllers/New1Controller.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day02Lab.Controllers
+{
+    public class New1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Sample()
+        {
+            return View();
+        }
+        public IActionResult Error() {
+            return View();
+        }
+    }
+}
---
+++ Day02Lab/Controllers/New2Controller.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day02Lab.Controllers
+{
+    public class New2Controller : Controller
+    {
+        public IActionResult TrangChu()
+        {
+            return View();
+        }
+        public IActionResult GioiThieu()
+        {
+            return View();
+        }
+    }
+}
