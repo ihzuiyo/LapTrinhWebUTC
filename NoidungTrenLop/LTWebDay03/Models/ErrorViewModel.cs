cfade6e64af1544f42a66c81df14b1a3426d4329
DangHoangHuy_ihz <159571472+ihzuiyo@users.noreply.github.com>
2025-09-18 19:46:33 +0700

    ------

--- NoidungTrenLop/LTWebDay03/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LTWebDay03.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
