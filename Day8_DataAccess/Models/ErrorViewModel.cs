d9d278d7bf24551b963415f0eaf1605f6b514a4e
DangHoangHuy_ihz <159571472+ihzuiyo@users.noreply.github.com>
2025-10-08 17:05:44 +0700

    Nộp nhầm file nên xóa

--- Day8_DataAccess/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Day8_DataAccess.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
